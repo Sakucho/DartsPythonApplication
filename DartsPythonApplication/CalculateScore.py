c7b80047282f63462655bd9899fbcaaabaaee37a
Sakucho <reception@shusakuhanamura.jp>
2022-09-29 22:49:36 +0900

    ダーツ盤にブル（50点）を追加

--- DartsPythonApplication/CalculateScore.py
+++ DartsPythonApplication/CalculateScore.py
@@ -2,7 +2,7 @@
 import random
 
 def board_to_point(s):
-
+    
     n_fold = s[0]
     point = int(s[1:])
 
@@ -19,13 +19,20 @@ if __name__ == '__main__':
     darts_board = [ 20,  1, 18,  4, 13,  6, 10, 15,  2, 17,
                      3, 19,  7, 16,  8, 11, 14,  9, 12,  5]
 
+    darts_board_exist_bull = [ 20,  1, 18,  4, 13,  6, 10, 15,  2, 17,
+                     3, 19,  7, 16,  8, 11, 14,  9, 12,  5, 50]
+
     n_fold = ['S', 'D', 'T']
 
     log = []
     score = 0
 
     for i in range(3):
-        log.append( random.choice(n_fold) + str(random.choice(darts_board)))
+        rand = random.choice(darts_board_exist_bull)
+        if rand == 50:
+            log.append('S50')
+        else:    
+            log.append( random.choice(n_fold) + str(random.choice(darts_board)))
         score += board_to_point(log[i])
 
     print(log)
